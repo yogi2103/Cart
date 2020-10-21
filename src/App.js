a752ae1669bc17b5e2f52b92fb23782717aa2c6a
Kabira Singh <yogesh.baghel86@gmail.com>
2020-10-21 07:38:54 +0530

    Add cartitem Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Cartitem from './Cartitem'; 
 function App() {
   return (
     <div className="App">
       <h1>Cart</h1>
+      <Cartitem/>
     </div>
   );
 }
